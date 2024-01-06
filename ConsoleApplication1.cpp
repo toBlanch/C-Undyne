2c8b783bd22675f05fdd96b7c21886b636836796
Jamie Blanchard <17b23802@marling.school>
2024-01-06 21:39:49 +0000

    Optimisation 2
    Changed factors such as HDC to further improve the code

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -5,34 +5,35 @@
 #include "ConsoleApplication1.h"
 
 using namespace std;
+HDC hdcScreen = ::GetDC(NULL);
 
 int* GetPixel(int x, int y) {
-    HDC hdcScreen = ::GetDC(NULL);
+    
     COLORREF pixel = ::GetPixel(hdcScreen, x, y);
-    ReleaseDC(NULL, hdcScreen);
+    //ReleaseDC(NULL, hdcScreen);
     int red = GetRValue(pixel);
     int green = GetGValue(pixel);
-    int blue = GetBValue(pixel);
-    int pixelColors[3] = { red, green, blue };
+    //int blue = GetBValue(pixel);
+    //int pixelColors[3] = { red, green, blue };
+    int pixelColors[2] = { red, green };
     return pixelColors;
 }
 
 int midpointX = 960;
 int midpointY = 540;
-int black = 20; //If a colour is less than or equal to this
+int black = 70; //If a colour is less than or equal to this
 int shieldR1 = 35;
 int shieldR2 = 65;
 int shieldG1 = 50;
 int shieldG2 = 70;
 int lastDirection = 0; //1=Left, 2=Right, 3=Down, 4=Up
-int i = 80;
+int innerI = 83;
 
-bool IfSpear(int colour[3]) {
-    bool isBlack = colour[0] <= black && colour[1] <= black; //If the colour is the background(black). B is not included as sometimes black is(0, 0, 70)
-    bool isWhite = colour[0] == colour[1] && colour[1] == colour[2]; //All 3 colours being the same is only true for the border of the box and for the characters
-    bool isShield = colour[0] >= shieldR1 && colour[0] <= shieldR2 && colour[1] >= shieldG1 && colour[1] <= shieldG2; //If the colour is the blue shield
-    return !(isBlack || isWhite || isShield);
-    //IE return  if not (If the colour is the background(black), if and ''''''
+bool IsSpear(int colour[2]) {
+    return colour[0] > black || colour[1] > black; //If the colour is the background(black). B is not included as sometimes black is(0, 0, 70)
+    //bool isWhite = colour[0] == colour[1] && colour[1] == colour[2]; //All 3 colours being the same is only true for the border of the box and for the characters
+    //bool isShield = colour[0] >= shieldR1 && colour[0] <= shieldR2 && colour[1] >= shieldG1 && colour[1] <= shieldG2; //If the colour is the blue shield
+    //return !(isBlack || isWhite || isShield);
 }
 
 void PressKey(int key) {
@@ -52,35 +53,35 @@ int main()
 {
     while (true) {
         if (lastDirection != 1) {
-            if (IfSpear(GetPixel(midpointX - i, midpointY))) {
-                PressKey(0x25);
+            if (IsSpear(GetPixel(midpointX - innerI, midpointY))) {
+                //PressKey(0x25);
+                printf("Left");
                 lastDirection = 1;
                 continue;
-                //printf("Left");
             }
         }
         if (lastDirection != 2) {
-            if (IfSpear(GetPixel(midpointX + i, midpointY))) {
-                PressKey(0x27);
+            if (IsSpear(GetPixel(midpointX + innerI, midpointY))) {
+                //PressKey(0x27);
+                printf("Right");
                 lastDirection = 2;
                 continue;
-                //printf("Right");
             }
         }
         if (lastDirection != 3) {
-            if (IfSpear(GetPixel(midpointX, midpointY - i))) {
-                PressKey(0x26);
+            if (IsSpear(GetPixel(midpointX, midpointY - innerI))) {
+                //PressKey(0x26);
+                printf("Up");
                 lastDirection = 3;
                 continue;
-                //printf("Up");
             }
         }
         if (lastDirection != 4) {
-            if (IfSpear(GetPixel(midpointX, midpointY + i))) {
-                PressKey(0x28);
+            if (IsSpear(GetPixel(midpointX, midpointY + innerI))) {
+                //PressKey(0x28);
+                printf("Down");
                 lastDirection = 4;
                 continue;
-                //printf("Down");
             }
         }
     }
